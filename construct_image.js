e441b577d4cc422158fdaabb28509625ce4f5011
Simon Rönnqvist <simon@iki.fi>
2011-01-08 02:07:23 +0200

    handling situation after the last strip

--- construct_image.js
+++ construct_image.js
@@ -1,6 +1,13 @@
 function constructImage(daysAgo) {
-  var img = document.createElement("image");
-  img.src = strips[daysAgo];
-  img.id = 'strip';
-  return img;
+  if (daysAgo < strips.length) {
+    var img = document.createElement("image");
+    img.src = strips[daysAgo];
+    img.id = 'strip';
+    return img;
+  } else {
+    var message = document.createElement("p");
+    message.id = 'strip';
+    message.innerHTML = 'För äldre Kaffi-strippar, se <a href="http://serier.nu/kaffi/" target="_blank">arkivet</a>.';
+    return message;
+  }
 }
